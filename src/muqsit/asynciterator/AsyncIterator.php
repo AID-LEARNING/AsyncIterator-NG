a35fb59e92a2d1a57372786a24ca876481010625
SenseiTarzan <gabrielcaptari0@gmail.com>
2024-11-03 13:59:21 +0100

    add function call for mode async enable

--- src/muqsit/asynciterator/AsyncIterator.php
+++ src/muqsit/asynciterator/AsyncIterator.php
@@ -7,6 +7,7 @@ namespace muqsit\asynciterator;
 use muqsit\asynciterator\handler\AsyncForeachHandler;
 use muqsit\asynciterator\handler\SimpleAsyncForeachHandler;
 use Iterator;
+use muqsit\asynciterator\handler\SimpleAsyncForeachHandlerGenerator;
 use pocketmine\scheduler\TaskScheduler;
 
 class AsyncIterator{
@@ -25,6 +26,21 @@ class AsyncIterator{
 	 */
 	public function forEach(Iterator $iterable, int $entries_per_tick = 10, int $sleep_time = 1) : AsyncForeachHandler{
 		$handler = new SimpleAsyncForeachHandler($iterable, $entries_per_tick);
+		$task_handler = $this->scheduler->scheduleDelayedRepeatingTask(new AsyncForeachTask($handler), 1, $sleep_time);
+		$handler->init("Plugin: {$task_handler->getOwnerName()} Event: AsyncIterator");
+		return $handler;
+	}
+
+	/**
+	 * @template TKey
+	 * @template TValue
+	 * @param Iterator<TKey, TValue> $iterable
+	 * @param int $entries_per_tick
+	 * @param int $sleep_time
+	 * @return AsyncForeachHandler<TKey, TValue>
+	 */
+	public function forEachGenerator(Iterator $iterable, int $entries_per_tick = 10, int $sleep_time = 1) : AsyncForeachHandler{
+		$handler = new SimpleAsyncForeachHandlerGenerator($iterable, $entries_per_tick);
 		$task_handler = $this->scheduler->scheduleDelayedRepeatingTask(new AsyncForeachTask($handler), 1, $sleep_time);
 		$handler->init("Plugin: {$task_handler->getOwnerName()} Event: AsyncIterator");
 		return $handler;
